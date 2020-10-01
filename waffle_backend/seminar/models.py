78232817dc30b0fdfd56ce123e3b1006f8d5df8e
231 <snu231@github.com>
2020-10-01 22:35:26 +0900

    black

---
+++ waffle_backend/seminar/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+
+from django.db import models
+
+# Create your models here.
+
+class Seminar(models.Model):
+    name = models.CharField(max_length=50)
+
+    joined_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.booleanField()    #
+
+    dropped_at = models.DateTimeField(auto_now=True)
+
+    # sem = SeminarManager()
+    # users = models.ManyToManyField(User, related_name = 'seminars', through = UserSeminar)
+
+class UserSeminar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    seminar = models.ForeignKey(Seminar, on_delete=models.CASCADE)
+    # rating = models.PositiveSmallIntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # joined_at = models.DateTimeField(auto_now=True)
+    # Seminar.sem.create_seminar()
+    # Seminar.sem.all()
+
+class ParticipantProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE , primary_key =True)
+
+    university = models.CharField(max_length=50, db_index=True)
+    accepted =models.booleanField()      # bool
+
+    seminars=models.ForeignKey( Seminar, null=True, related_name='seminar', on_delete=models.SET_NULL)  # related_name
+
+    # rating = models.PositiveSmallIntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class InstructorProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
+
+    company = models.CharField(max_length=50, db_index=True)
+    year = models.PositiveSmallIntegerField(null=True)   #  null?
+
+    charge = models.ForeignKey( Seminar, null=True, related_name='seminar', on_delete=models.SET_NULL)  #  related_name
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
